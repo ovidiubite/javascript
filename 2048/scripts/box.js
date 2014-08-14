f5308c00538acca8e30dd362d1b19d0ec489ade1
ovidiubite <ovidiu_bite10@hotmail.com>
2014-08-14 15:02:07 +0300

    createPiece

--- 2048/scripts/box.js
+++ 2048/scripts/box.js
@@ -1,31 +1,41 @@
 $(document).ready(function(){
+	createPiece();
 	$(document).keydown(function(key){ 
-			var $square = $("#square");
-			var $posT = $square.position().top;
-			var $posL = $square.position().left;
+			var $square = $(".square");
+			var $posTop = $square.position().top;
+			var $posLeft = $square.position().left;
 		   switch(parseInt(key.which,10)){
 			// right
 			case 39:
-				if ($posL < 300)
-				$square.animate({left: "+=100px"}, 1);
+				if ($posLeft < 300)
+				$square.animate({left: "+=106px"}, 1);
+				createPiece();
 				break;
 			// Up
 			case 38:
-				if ($posT > 0)				
-				$square.animate({top: "-=100px"}, 1);
+				if ($posTop > 0)				
+				$square.animate({top: "-=106px"}, 1);
+				createPiece();
 				break;
 			// left
 			case 37:
-				if ($posL > 0)				
-				$square.animate({left: "-=100px"}, 1);
+				if ($posLeft > 0)				
+				$square.animate({left: "-=106px"}, 1);
+				createPiece();
 				break;
 			// Down
 			case 40:
-				if ($posT < 300)				
-				$square.animate({top: "+=100px"}, 1);
+				if ($posTop < 300){
+				$square.animate({top: "+=106px"}, 1);
+				createPiece();
+				}
 				break;
 			}
 	});
 });
 
 
+var createPiece = function(){
+	$('#screen').append('<div class="square"></div>');
+}
+ 
